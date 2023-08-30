d4556469da7ccbcefdad6bb97a8984c51e801fd8
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-08-30 18:50:57 +0530

    calculaor

---
+++ calulator/index.js
@@ -0,0 +1,34 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+}
+
